db810c7720d405914a373787e8206fa602693ee8
Koen Veldhorst <koen.veldhorst@student.uva.nl>
2024-04-22 14:25:15 +0200

    additional padding exec-time experiments

--- learning_pytorch/padding_comparison.py
+++ learning_pytorch/padding_comparison.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from itertools import product
 from timeit import timeit
 import numpy as np
+import csv
 
 import torch.nn as nn
 import torch
@@ -33,7 +34,7 @@ def calc_exec_time(f, w):
     funcs = [unf_pad, pad_unf]
     n_itters = [100, 1000, 10000]
 
-    itterations = 1000
+    itterations = 100
     for n, func in product(n_itters, funcs):
         exec_time[(func.__name__, n)] = [timeit(lambda: func(f, w), number=n)]
         for _ in range(itterations - 1):
@@ -93,12 +94,36 @@ def plot_exec_time(f, w):
     plt.show()
 
 
+def write_exec_time_to_csv(data):
+    csv_file_path = 'padding_execution_times.csv'
+
+    # Writing to CSV
+    with open(csv_file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        # Writing the headers
+        writer.writerow(['Feature Map Size', 'Operation', 'Iterations', 'Mean Execution Time', 'Standard Deviation'])
+
+        # Writing the data
+        for feature_map_size, operations in data.items():
+            for (operation, iterations), (mean, std) in operations.items():
+                writer.writerow([f"{feature_map_size[0]}x{feature_map_size[1]}", operation, iterations, mean, std])
+
+    print(f"Data written to {csv_file_path}")
+
+
 if __name__ == "__main__":
+
+    # mean_std_exec_times = {}
+    # for height, width in [(8, 8), (32, 32), (256,256)]:
+    #     f = torch.arange(1, width**2 + 1, dtype=torch.float32).view(1, 1, height, width)
+    #     w = torch.arange(1, 10, dtype=torch.float32).view(1, 1, 3, 3)
+
+    #     mean_std_exec_time, n_itters = calc_exec_time(f, w)
+    #     mean_std_exec_times[(height, width)] = mean_std_exec_time
+
+    # write_exec_time_to_csv(mean_std_exec_times)
+
     f = torch.arange(1, F_W**2 + 1, dtype=torch.float32).view(1, 1, F_H, F_W)
     w = torch.arange(1, 10, dtype=torch.float32).view(1, 1, 3, 3)
-
-    first_order = unf_pad(f, w)
-    second_order = pad_unf(f, w)
-    print(torch.allclose(first_order, second_order))
 
     plot_exec_time(f, w)
