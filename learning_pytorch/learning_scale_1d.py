17782634ac5d45d4896a1b336d56057f562ba65f
Koen Veldhorst <koen.veldhorst@student.uva.nl>
2024-05-15 22:10:03 +0200

    example how a scale parameter for a one dimensional tensor can be learned

--- learning_pytorch/learning_scale_1d.py
+++ learning_pytorch/learning_scale_1d.py
@@ -2,6 +2,9 @@ import torch.nn.functional as F
 import torch.nn as nn
 import torch
 
+# set seed for reproducibility
+torch.manual_seed(0)
+
 
 def create_kernel(kernel_size, scale):
     z_i = torch.linspace(-kernel_size // 2 + 1, kernel_size // 2, kernel_size, dtype=torch.float32)
@@ -56,24 +59,27 @@ def maxvalues(a, dim=1):
 semifield = (maxvalues, torch.add, -1 * torch.inf, 0)
 # semifield = (torch.sum, torch.mul, 0, 1)
 
-input_tensor = torch.tensor([2*x for x in range(5)], dtype=torch.float32).view(1, 1, 1, 5)
+ks = 3
+
+input_tensor = torch.tensor([2*x for x in range(6)], dtype=torch.float32).view(1, 1, 1, 6)
 print(f'Input: {input_tensor}')
-target_tensor = SemiConvModel(semifield=semifield, input_channels=1, output_channels=1, kernel_size=3, s=1.0)(input_tensor).clone().detach()
+target_tensor = SemiConvModel(semifield=semifield, input_channels=1, output_channels=1, kernel_size=ks, s=1.0)(input_tensor).clone().detach()
 print(f'Target: {target_tensor}')
 
-model = SemiConvModel(semifield=semifield, input_channels=1, output_channels=1, kernel_size=3, s=0.9)
+model = SemiConvModel(semifield=semifield, input_channels=1, output_channels=1, kernel_size=ks, s=0.5)
 criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-num_epochs = 100
+num_epochs = 1000
 for epoch in range(num_epochs):
     optimizer.zero_grad()
     output = model(input_tensor)
     loss = criterion(output, target_tensor)
     loss.backward()
     optimizer.step()
-    if (epoch + 1) % 10 == 0:
-        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item()}, Scale: {model.scale.item()}')
+    if (epoch + 1) % 100 == 0:
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item()}, Scale: {model.scale.item()}, Grad: {model.scale.grad.item()}')
 
 # Test the learned scale parameter
 print(f'Learned Scale Parameter: {model.scale.item()}')
+print(f'Output: {model(input_tensor).detach()}')
